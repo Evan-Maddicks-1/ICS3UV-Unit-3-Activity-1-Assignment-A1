eac1fadbca75f31848c7405f4003d6d86131233e
default <evan.maddicks@stu.ocsb.ca>
2025-11-14 14:08:50 +0000

    Redid entire program to ensure linter works properly

---
+++ main.ts
@@ -0,0 +1,10 @@
+/**
+ * @author Evan Maddicks
+ * @version 1.0.0
+ * @date 2025-11-14
+ * @fileoverview The program will calculate the area of a circle using the given radius
+ */
+let radius = 5.6
+let area = 3.14 * (radius * radius)
+
+console.log("The area of a circle with the radius " + radius + " is " + area + " cm²" )
